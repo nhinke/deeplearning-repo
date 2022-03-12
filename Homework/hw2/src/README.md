081b813af54d6559a716312ab8a3839c19f73f73
Nick Hinke <nhinke2@gmail.com>
2022-03-12 00:47:26 -0500

    finished constructing 10 binary classifiers (using perceptrons) for MNIST handwritten digits as part of assignment 2

---
+++ Homework/hw2/src/README.md
@@ -0,0 +1,9 @@
+## 520.638 - Deep Learning
+### Homework 2 - Handwritten Digits using Perceptron and Logistic Regression
+<br>
+In this directory, there are three files of note: <br>
+TODO: UPDATE
+1. dataset.mat  --  contains Extended YaleB dataset (32x32 images) <br>
+2. problem2.py  --  contains all of the source code used for problem 2 <br>
+3. problem3.py  --  contains all of the source code used for problem 3 <br>
+
---
+++ Homework/hw2/src/problem2.py
@@ -0,0 +1,98 @@
+# Nick Hinke
+# 520.638 Deep Learning
+# Homework 2 - Problem 2
+#
+#
+#
+
+# import relevant libraries
+import gzip
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import Perceptron
+
+# function to extract datasets and labels as numpy arrays from the four .gz folders available at http://yann.lecun.com/exdb/mnist/
+def get_datasets_from_gz_folders(im_size, num_test, num_train):
+
+    # try to load datasets from .gz folders
+    try:
+        training_images = gzip.open('train-images-idx3-ubyte.gz','rb')
+        training_labels = gzip.open('train-labels-idx1-ubyte.gz','rb')
+        test_images = gzip.open('t10k-images-idx3-ubyte.gz','rb')
+        test_labels = gzip.open('t10k-labels-idx1-ubyte.gz','rb')
+    except:
+        print("Please ensure that the four MNIST .gz folders are saved in the correct directory.")
+        quit()
+    
+    # convert training images to numpy array of shape (num_train,im_size*im_size)
+    training_images.read(16)
+    buf = training_images.read(im_size*im_size*num_train)
+    training_images = np.frombuffer(buf,dtype=np.uint8).astype(np.float32)
+    training_images = training_images.reshape(-1,im_size*im_size)
+    
+    # convert training labels to numpy array of shape (num_train,)
+    training_labels.read(8)
+    buf = training_labels.read(num_train)
+    training_labels = np.frombuffer(buf,dtype=np.uint8)
+
+    # convert test images to numpy array of shape (num_test,im_size*im_size)
+    test_images.read(16)
+    buf = test_images.read(im_size*im_size*num_test)
+    test_images = np.frombuffer(buf,dtype=np.uint8).astype(np.float32)
+    test_images = test_images.reshape(-1,im_size*im_size)
+
+    # convert test labels to numpy array of shape (num_test,)
+    test_labels.read(8)
+    buf = test_labels.read(num_test)
+    test_labels = np.frombuffer(buf,dtype=np.uint8)
+
+    return training_images, training_labels, test_images, test_labels
+
+
+def main():
+
+    # define image size and number of images to keep from each dataset
+    image_size = 28
+    num_test_samples = 2000
+    num_training_samples = 20000
+
+    # get datasets as numpy arrays with the following shapes: {x_train,y_train} = {(num_training_samples,784),(num_training_samples,)} and {x_test,y_test} = {(num_test_samples,784),(num_test_samples,)}
+    x_train, y_train, x_test, y_test = get_datasets_from_gz_folders(image_size, num_test_samples, num_training_samples)
+    
+    # reshape images from row vectors to 28x28 matrices for visualization purposes (e.g. plt.imshow(x_test_images[image_index]))
+    x_test_images = x_test.reshape(num_test_samples, image_size, image_size, 1)
+    x_train_images = x_train.reshape(num_training_samples, image_size, image_size, 1)
+    
+    # append a column of ones to the matrices of images corresponding to the bias term
+    x_test = np.hstack((np.ones(shape=(num_test_samples,1)),x_test))
+    x_train = np.hstack((np.ones(shape=(num_training_samples,1)),x_train))
+
+    # train 10 binary classifiers (using perceptrons) where the i-th classifier predicts whether or not the digit in the image = i
+    num_digits = 10
+    perceptron_list = list()
+    for id in range(num_digits):
+        y_train_id = [1 if yi == id else 0 for yi in y_train]
+        perceptron_list.append(Perceptron().fit(x_train,y_train_id))
+        print("Done with training classifier for digit " + str(id) + "...")
+
+    # compute classification errors on test dataset for each binary classifier
+    test_error_list = list()
+    print("Computing test errors for all classifers...")
+    for id,clf in enumerate(perceptron_list):
+        y_test_id = [1 if yi == id else 0 for yi in y_test]
+        y_pred = clf.predict(x_test)
+        err = sum([1 for id,y_val in enumerate(y_test_id) if y_pred[id] != y_val])*100.0/(1.0*num_test_samples) # error percentage
+        test_error_list.append(err)
+    
+    # print(test_error_list)
+
+    # print()
+    # im_nums=[738,18,92,47,1,99,1932,267,1345,1234,111,1111,897,777,0]
+    # for im_num in im_nums:
+    #     print(y_test[im_num])
+    #     for clf in perceptron_list:
+    #         print(clf.predict(x_test[im_num].reshape(1,-1)))
+
+
+if __name__ == "__main__":
+	main()
