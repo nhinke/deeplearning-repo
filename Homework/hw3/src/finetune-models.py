c44ae58b2f709181fc3e7e414ad09356824d798e
Nick Hinke <nhinke2@gmail.com>
2022-04-12 15:54:31 -0400

    completed scripts required for assignment 3 finetuning

--- Homework/hw3/src/finetune-models.py
+++ Homework/hw3/src/finetune-models.py
@@ -2,11 +2,11 @@
 # 520.638 Deep Learning
 # Homework 3 - Problem 2
 #
-#
+# Script used to fine-tune AlexNet or VGG16 pre-trained model for verification task on LFW dataset
+# Script will save resulting model state_dict to location specified by 'NETWORK_PARAM_DIRECTORY'
 #
 
-# import numpy as np
-
+import os
 import torch
 import torchvision.models as models
 import torchvision.datasets as datasets
@@ -15,26 +15,43 @@ from torch.autograd import Variable as Variable
 
 def main():
 
-    USE_VGG16 = False
-    USE_ALEXNET = True
+    USE_CPU = False
+
+    USE_VGG16 = True
+    USE_ALEXNET = not USE_VGG16
 
     USE_LFWPAIRS_DATASET = True
-    USE_LFWPEOPLE_DATASET = False
+    USE_LFWPEOPLE_DATASET = not USE_LFWPAIRS_DATASET
 
     DATASET_DIRECTORY = '/home/nhinke/Documents/JHU/Robotics-MSE/S22/DL/Coursework/Homework/hw3/data'
     NETWORK_PARAM_DIRECTORY = '/home/nhinke/Documents/JHU/Robotics-MSE/S22/DL/Coursework/Homework/hw3/data'
 
-    epochs = 10
-    momentum = 0.9
-    batch_size = 22
-    learningRate = 0.0001
-    percent_validation = 0.01
+    percent_validation = 0.01 
+    USE_VALIDATION_SET = False
+       
+    if (USE_VGG16):
+        epochs = 5
+        momentum = 0.9
+        batch_size = 4
+        learningRate = 0.0001
+        num_epochs_to_update_conv_layers = 2
+    elif (USE_ALEXNET):
+        epochs = 20
+        momentum = 0.9
+        batch_size = 22
+        learningRate = 0.0001
+        num_epochs_to_update_conv_layers = 15
 
-    # all params trainable, learningRate .0001, epochs = 10 --> auc = .8093
 
     # check if GPU if available
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(f'\nDevice: {device}\n')
+    if (not USE_CPU):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        if (device.type == 'cuda'):
+            torch.cuda.empty_cache()
+            os.environ['PYTORCH_CUDA_ALLOC_CONF']='max_split_size_mb:128'
+    else:
+        device = torch.device('cpu')
+    print(f'\nDevice: {device}')
 
     # check to make sure network type selected appropriately
     if (not USE_ALEXNET and not USE_VGG16):
@@ -54,9 +71,10 @@ def main():
 
     if (USE_ALEXNET):
         model = models.alexnet(pretrained=True)
-
+        model_str = 'AlexNet'
     elif (USE_VGG16):
         model = models.vgg16(pretrained=True)
+        model_str = 'VGG16'
 
     # define proper image transformations
     transform = transforms.Compose([transforms.Resize(256), 
@@ -68,33 +86,57 @@ def main():
     model.to(device)
     model.train(True)
 
+    # define function to compute euclidean distance between two tensors (using pdist(tensor1,tensor2))
     pdist = torch.nn.PairwiseDistance(p=2.0)
 
     # intialize loss function (Binary Cross Entropy) and optimizer (Stochastic Gradient Descent)
     criterion = torch.nn.BCELoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learningRate, momentum=momentum)
-    # optimizer = torch.optim.Adam(model.parameters(), lr=learningRate)
 
     if (USE_LFWPAIRS_DATASET):
+
+        print(f'Fine-tuning {model_str} pre-trained model on LFWPairs dataset...\n')
 
         # load total training set
         total_train_set = datasets.LFWPairs(root=DATASET_DIRECTORY, download=True, split='train', image_set='original', transform=transform)
 
         # split total training set into training and validation sets
-        num_valid = round(percent_validation*total_train_set.__len__())
-        num_train = total_train_set.__len__() - num_valid
-        split_train_set, split_valid_set = torch.utils.data.random_split(total_train_set, [num_train,num_valid])
-        train_loader = torch.utils.data.DataLoader(split_train_set, batch_size=batch_size, shuffle=True, num_workers=4)
-        # valid_loader = torch.utils.data.DataLoader(split_valid_set, batch_size=batch_size, shuffle=False, num_workers=4)
-
-        for param in model.features.parameters():
-            param.requires_grad = True 
+        if (USE_VALIDATION_SET):
+            num_valid = round(percent_validation*total_train_set.__len__())
+            num_train = total_train_set.__len__() - num_valid
+            split_train_set, split_valid_set = torch.utils.data.random_split(total_train_set, [num_train,num_valid])
+            train_loader = torch.utils.data.DataLoader(split_train_set, batch_size=batch_size, shuffle=True, num_workers=4)
+            valid_loader = torch.utils.data.DataLoader(split_valid_set, batch_size=batch_size, shuffle=False, num_workers=4)
+            num_train_samples = split_train_set.__len__()
+        else:
+            train_loader = torch.utils.data.DataLoader(total_train_set, batch_size=batch_size, shuffle=True, num_workers=4)
+            num_train_samples = total_train_set.__len__()
+        
+        # freeze parameters within convolutional layers (model.features.parameters()) if 'num_epochs_to_update_conv_layers' set to less than total number of epochs
+        if (num_epochs_to_update_conv_layers < epochs):
+            conv_param_flag = False
+            for param in model.features.parameters():
+                param.requires_grad = False 
+            for param in model.classifier.parameters():
+                param.requires_grad = True 
+        else:
+            conv_param_flag = True
+            for param in model.features.parameters():
+                param.requires_grad = True 
+            for param in model.classifier.parameters():
+                param.requires_grad = True 
 
         # train model
+        eps = 0.01
         max_dissimilarity = -1.0
-        one_tensor = Variable(torch.tensor([1.0]).to(device))
-        zero_tensor = Variable(torch.tensor([0.0]).to(device))
+        num_mini_batches = round(1.0*num_train_samples/batch_size/10.0)
         for epoch in range(epochs):
+            # unfreeze parameters in convolutional layers as specified by 'num_epochs_to_update_conv_layers' 
+            if (not conv_param_flag and epoch >= (epochs-num_epochs_to_update_conv_layers)):
+                conv_param_flag = True
+                for param in model.features.parameters():
+                    param.requires_grad = True
+            # reset running_loss statistic to zero
             running_loss = 0.0
             for id,data in enumerate(train_loader):
                 # load batch of image pairs and labels and move to GPU as appropriate
@@ -107,35 +149,30 @@ def main():
                 output1 = model(input1)
                 output2 = model(input2)
                 # compute dissimilarity measure for each image pair within batch and "normalize" it such that identical images have a dissimilarity of 0.0 and the most dissimilar images have a dissimilarity of 1.0
-                batch_dissimilarity = pdist(output1,output2)
+                batch_dissimilarity = pdist(output1,output2) + eps
                 torch_max = torch.max(batch_dissimilarity).cpu().data.numpy().reshape(-1)[0]
                 if (max_dissimilarity < torch_max):
                     max_dissimilarity = torch_max
-                batch_dissimilarity_normalized = torch.div(batch_dissimilarity,max_dissimilarity)
-                batch_dissimilarity_normalized = torch.maximum(torch.minimum(batch_dissimilarity_normalized,one_tensor),zero_tensor)
-
-                # batch_dissimilarity = np.linalg.norm(output1.cpu().data.numpy() - output2.cpu().data.numpy(), axis=1)
-                # if (max_dissimilarity < np.max(batch_dissimilarity)):
-                #     max_dissimilarity = np.max(batch_dissimilarity)
-                # if (min_dissimilarity > np.min(batch_dissimilarity)):
-                #     min_dissimilarity = np.min(batch_dissimilarity)
-                # batch_dissimilarity = 1.0*batch_dissimilarity/max_dissimilarity
-                # batch_dissimilarity = 1.0*(batch_dissimilarity-min_dissimilarity)/(max_dissimilarity-min_dissimilarity)
-                # batch_dissimilarity = torch.from_numpy(batch_dissimilarity).to(device)
-                # batch_dissimilarity = Variable(batch_dissimilarity,requires_grad=True)
-                
-                # perform backpropagation using specified loss function 
+                batch_dissimilarity_normalized = torch.div(batch_dissimilarity,max_dissimilarity+eps)
+                # perform backpropagation using specified loss function and gradient clipping to prevent exploding gradients
                 loss = criterion(batch_dissimilarity_normalized,labels)
                 loss.backward()
+                torch.nn.utils.clip_grad_norm_(model.parameters(),max_norm=100.0)
                 optimizer.step()
                 running_loss += loss.item()
-                # print and reset running_loss statistic every 10 batches
-                if ((id+1) % 10 == 0):   
-                    print(f'[{epoch+1},{id+1:3d}] loss: {running_loss/10.0:.3f}')
+                # print and reset running_loss statistic every num_mini_batches batches
+                if ((id+1) % num_mini_batches == 0):   
+                    print(f'[{epoch+1},{id+1:3d}] loss: {1.0*running_loss/num_mini_batches:.3f}')
                     running_loss = 0.0
             print(f'EPOCH {epoch+1}/{epochs} COMPLETE...\n')
 
     elif (USE_LFWPEOPLE_DATASET):
+
+        # todo
+        print("Error: please set 'USE_LFWPAIRS_DATASET' boolean to 'True' at top of main()")
+        quit()
+
+        print(f'Fine-tuning {model_str} pre-trained model on LFWPeople dataset...\n')
 
         # load total training set
         total_train_set = datasets.LFWPeople(root=DATASET_DIRECTORY, download=True, split='train', image_set='original', transform=transform)
@@ -147,6 +184,7 @@ def main():
         network_param_path = NETWORK_PARAM_DIRECTORY + '/vgg16-finetuned.pth'
     print(f'Finished training!\nSaving model parameters to: {network_param_path}\n')
     torch.save(model.state_dict(),network_param_path)
+
 
 if __name__ == "__main__":
     main()
--- Homework/hw3/src/problem2.py
+++ Homework/hw3/src/problem2.py
@@ -2,7 +2,9 @@
 # 520.638 Deep Learning
 # Homework 3 - Problem 2
 #
-#
+# Script used to test model performance on verification task using LFW dataset
+# Script will load pre-trained AlexNet or VGG16 model, or fine-tuned AlexNet or VGG16 model as specified by 'WITH_FINE_TUNING'
+# Please refer to finetune-models.py script for details regarding process of fine-tuning the pre-trained models, or to actually fine-tune and save the model parameters as needed
 #
 
 import math
@@ -11,7 +13,6 @@ import matplotlib.pyplot as plt
 import sklearn.metrics as metrics
 
 import torch
-# import torchvision
 import torchvision.models as models
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
@@ -46,7 +47,9 @@ def main():
 
     WITH_FINE_TUNING = True
 
-    USE_VGG16 = False
+    USE_CPU = False
+
+    USE_VGG16 = True
     USE_ALEXNET = not USE_VGG16
 
     USE_LFWPAIRS_DATASET = True
@@ -57,8 +60,11 @@ def main():
 
 
     # check if GPU if available
-    device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    print(f'\nDevice: {device}\n')
+    if (not USE_CPU):
+        device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+    else:
+        device = 'cpu'
+    print(f'\nDevice: {device}')
 
     # check to make sure network type selected appropriately
     if (not USE_ALEXNET and not USE_VGG16):
@@ -86,9 +92,11 @@ def main():
                 print(f"Error: please ensure finetuned AlexNet model saved at '{NETWORK_PARAM_DIRECTORY}/alexnet-finetuned.pth'")
                 quit()
             plot_title = 'AlexNet (with Fine-Tuning)'
+            model_str = 'fine-tuned AlexNet'
         else:
             model = models.alexnet(pretrained=True)
             plot_title = 'AlexNet (without Fine-Tuning)'
+            model_str = 'pre-trained AlexNet'
     elif (USE_VGG16):
         if (WITH_FINE_TUNING):
             try:
@@ -98,21 +106,25 @@ def main():
                 print(f"Error: please ensure finetuned VGG16 model saved at '{NETWORK_PARAM_DIRECTORY}/vgg16-finetuned.pth'")
                 quit()
             plot_title = 'VGG16 (with Fine-Tuning)'
+            model_str = 'fine-tuned VGG16'
         else:
             model = models.vgg16(pretrained=True)
             plot_title = 'VGG16 (without Fine-Tuning)'
+            model_str = 'pre-trained VGG16'
 
     # define proper image transformations
     transform = transforms.Compose([transforms.Resize(256), 
                                     transforms.CenterCrop(224),   
                                     transforms.ToTensor(),  
                                     transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])])
-
+    
     # move selected model to GPU if available, and set to evaluation mode
     model.to(device)
     model.eval()
 
     if (USE_LFWPAIRS_DATASET):
+
+        print(f'Testing {model_str} model on LFWPairs dataset...\n')
 
         # set dataset name
         dataset_name = 'LFWPairs'
@@ -147,12 +159,15 @@ def main():
 
     elif (USE_LFWPEOPLE_DATASET):
 
+        print(f'Testing {model_str} model on LFWPeople dataset...\n')
+
         # set dataset name
         dataset_name = 'LFWPeople'
         
         # load dataset
         test_dataset = datasets.LFWPeople(root=DATASET_DIRECTORY, download=True, split='test', image_set='original', transform=transform)
         test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=36, shuffle=False, num_workers=4)
+        num_test_images = test_dataset.__len__()
 
         # initialize lists to store output labels (individual identities) and output feature vectors (dim=1000x1) for each image
         output_features = list()
@@ -160,7 +175,7 @@ def main():
 
         # compute model outputs and store image identities for each image in dataset
         with torch.no_grad():
-            print('Computing model outputs for each image in dataset...')
+            print(f'Computing model outputs for all {num_test_images} images in test dataset...')
             for i,data in enumerate(test_loader):
                 # load batch of images and identity labels and move to GPU as appropriate
                 inputs, labels = data
@@ -184,15 +199,14 @@ def main():
         dissimilarity_vector = list()
 
         # loop over every possible pair of images in dataset
-        num_images = output_features.shape[0]
-        num_comparisons = math.comb(num_images,2)
+        num_comparisons = math.comb(num_test_images,2)
         pairs = 0
         percent = 0
         ten_percent = round(num_comparisons/10.0)
-        print(f'Computing dissimilarity measure for all {num_comparisons} pairs of images in dataset...')
+        print(f'Computing dissimilarity measure for all {num_comparisons} pairs of images in test dataset...')
         for id1,output1 in enumerate(output_features):
             label1 = output_identities[id1]
-            for id2 in range(id1+1, num_images):
+            for id2 in range(id1+1, num_test_images):
                 label2 = output_identities[id2]
                 output2 = output_features[id2]
                 # compare image identities for image pair and store as label, and compute and store dissimilarity measure for image pair
@@ -223,37 +237,6 @@ def main():
     plt.show()
 
 
-    
-    # load datasets
-    # data_path = '/home/nhinke/Documents/JHU/Robotics-MSE/S22/DL/Coursework/Homework/hw3/data'
-    # total_test_set = datasets.LFWPeople(root=data_path, download=True, split='test', image_set='original', transform=transform)
-    # total_train_set = datasets.LFWPeople(root=data_path, download=True, split='train', image_set='original', transform=transform)
-
-
-    # dataiter = iter(train_loader)
-    # images,labels = dataiter.next()
-    # print(labels)
-    # imshow(torchvision.utils.make_grid(images))
-
-
-    # plt.figure()
-    # thresholds = [0.20,0.25,0.30,0.35]
-    # y_predicted = list()
-    # for threshold in thresholds:
-    #     y_pred = [1 if diff < threshold else 0 for diff in dissimilarity_vector]
-    #     y_predicted.append(y_pred)
-    #     tpr,fpr = getROC(y_true,y_pred)
-    #     print(f'\nTPR:  {tpr}')
-    #     print(f'FPR:  {fpr}')
-    #     fpr, tpr, thr = metrics.roc_curve(y_true, y_pred, drop_intermediate=False)
-    #     roc_auc = metrics.auc(fpr, tpr)
-    #     print(f'TPR:  {tpr}')
-    #     print(f'FPR:  {fpr}')
-    #     print(f'THR:  {thr}')
-    #     plt.plot(fpr,tpr,label="th="+str(threshold)+", auc="+str(roc_auc))
-
-
-    
 
 if __name__ == "__main__":
     main()
